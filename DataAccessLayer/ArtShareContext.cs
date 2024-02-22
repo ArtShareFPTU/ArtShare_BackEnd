8ddef20f172841c1d840ee103eabd9a225e9607c
LNPhuc <lnphuc2006@gmail.com>
2024-02-22 14:57:05 +0700

    update

--- DataAccessLayer/ArtShareContext.cs
+++ DataAccessLayer/ArtShareContext.cs
@@ -32,7 +32,7 @@ public partial class ArtShareContext : DbContext
         if (!optionsBuilder.IsConfigured)
         {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            optionsBuilder.UseSqlServer("server =NHAPHAN\\SQLEXPRESS; database = ArtShare;uid=sa;pwd=1234567890;");
+            optionsBuilder.UseSqlServer("server =(local); database = ArtShare;uid=sa;pwd=1234567890;");
         }
     }
 
