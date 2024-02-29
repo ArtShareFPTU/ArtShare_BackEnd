9df9e433fa903aecb50dd10d3626fb69895b1f18
quynh <quynhphama99@gmail.com>
2024-02-29 16:41:29 +0700

    test-detail

---
+++ Presentation/wwwroot/css/DetailPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.wwwroot.css
+{
+    public class DetailPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
