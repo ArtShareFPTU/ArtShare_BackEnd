8e8454b3369281866671c32ca6a2a4af49f188dc
NhaPhan06 <phanthanhnha06@gmail.com>
2024-03-03 19:09:09 +0700

    paypal

--- DataAccessLayer/ArtShareContext.cs
+++ DataAccessLayer/ArtShareContext.cs
@@ -34,12 +34,13 @@ public partial class ArtShareContext : DbContext
         if (!optionsBuilder.IsConfigured)
         {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            optionsBuilder.UseSqlServer("server=(local);user=sa;password=1234567890;database=ArtShare;Trusted_Connection=True;TrustServerCertificate=True").EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlServer(GetConnectionStrings()).EnableSensitiveDataLogging();
         }
     }
-    
-    private string GetConnectionStrings() {
-        IConfigurationRoot config = new ConfigurationBuilder()
+
+    private string GetConnectionStrings()
+    {
+        var config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", true, true)
             .SetBasePath(Directory.GetCurrentDirectory())
             .Build();
