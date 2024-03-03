1e8b4a29e3c998750365b8ad8ef355b90ff192aa
NhaPhan06 <phanthanhnha06@gmail.com>
2024-03-03 17:18:01 +0700

    paypal

---
+++ ModelLayer/DTOS/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelLayer.DTOS
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
