13b4a04a5c1a77bcc6cbbe74daa08203d9174ca8
NhaPhan06 <phanthanhnha06@gmail.com>
2024-03-07 14:59:37 +0700

    .

--- Presentation/Pages/DetailPage.cshtml.cs
+++ Presentation/Pages/DetailPage.cshtml.cs
@@ -75,7 +75,7 @@ public class DetailPageModel : PageModel
         // serialize cart
         json = JsonConvert.SerializeObject(cartsList);
         HttpContext.Session.SetString("cart", json);
-        return RedirectToPage(new { id });;
+        return RedirectToPage(new { id });
     }
 
 
