cb0681f3740adbc5a1e7cd715d455dac4e0781dd
NhaPhan <phanthanhnha06@gmail.com>
2024-03-15 22:47:26 +0700

    Change background Color

--- DataAccessLayer/Repository/ArtworkRepository.cs
+++ DataAccessLayer/Repository/ArtworkRepository.cs
@@ -22,7 +22,7 @@ public class ArtworkRepository : IArtworkRepository
 
     public async Task<List<Artwork>> GetAllArtworkAsync()
     {
-        return await _context.Artworks.Include(a => a.Account).OrderByDescending(c => c.Fee).ToListAsync();
+        return await _context.Artworks.Include(a => a.Account).OrderByDescending(c => c.Likes).ToListAsync();
     }
 
     public async Task<Artwork> GetArtworkByIdAsync(Guid id)
